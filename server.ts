099baa8792422d3704ab6856c203ad1fff12debc
Ark-dev-lab <ravanda99@gmail.com>
2026-03-26 18:16:58 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import { createServer as createViteServer } from "vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = new URL('.', import.meta.url).pathname;
 
 async function startServer() {
   const app = express();
