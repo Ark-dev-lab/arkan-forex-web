75743874b35ecc189b294a3b8235f4deda6def65
Ark-dev-lab <ravanda99@gmail.com>
2026-03-26 16:10:36 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './components/App';
+import App from './components/App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
